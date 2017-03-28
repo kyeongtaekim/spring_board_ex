c8d7bcb01458d27bc718cf6476ecbbfee69587b0
junghun415 <junghun415@gmail.com>
2017-03-28 17:54:59 +0900

    Revert "안녕 디지몬"
    This reverts commit 760e84c589a0c4daf0b45cae2bae226431903e60.

--- (v.2)FinalSpringMVCBoard/src/main/java/com/javalec/spring_pjt_board_command/BCommand.java
+++
@@ -1,8 +0,0 @@
-package com.javalec.spring_pjt_board_command;
-
-import org.springframework.ui.Model;
-
-public interface BCommand {
-		public void execute(Model model);
-		
-}
--- (v.2)FinalSpringMVCBoard/src/main/java/com/javalec/spring_pjt_board_util/Constant.java
+++
@@ -1,7 +0,0 @@
-package com.javalec.spring_pjt_board_util;
-
-import org.springframework.jdbc.core.JdbcTemplate;
-
-public class Constant {
-		public static JdbcTemplate template;
-}
